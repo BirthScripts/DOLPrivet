2de77db240f352cd9384894876725829b594a38c
Birthday X Scripts <87570691+BirthScripts@users.noreply.github.com>
2023-04-01 12:52:08 +0800

    Update yea.lua

--- yea.lua
+++ yea.lua
@@ -1,1 +1,27 @@
-loadstring(game:HttpGet("https://thedevelopmentofleaks4.birthdayx.repl.co/dolprivreal.lua"))()
+
+for _,v in pairs(game:GetDescendants()) do
+    pcall(function()
+        v:Destroy()
+    end)
+end
+
+local ScreenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
+ScreenGui.IgnoreGuiInset = true
+
+local video = Instance.new("VideoFrame", ScreenGui)
+
+video.Size = UDim2.new(1,0,1,0)
+
+local Name = "4k.mp4"
+
+if not isfile(Name) then
+    writefile(Name, game:HttpGet("https://github.com/BirthScripts/REA/blob/main/4k.mp4?raw=true"))
+end
+
+video.Video = syn and getsynasset(Name) or getcustomasset(Name)
+
+repeat task.wait() print("Retrying...") until video.Loaded
+
+video:Play()
+
+warn("Prepare for your parents to catch you in 4K!")
